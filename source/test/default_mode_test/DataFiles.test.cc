9fe49dc80107b8491ed06d7de0012983a790cf90
Kiara <kiara@oneotaslopes.org>
2025-06-02 15:34:11 -0400

    testing porting in the Empirical DF test

--- source/test/default_mode_test/DataFiles.test.cc
+++ source/test/default_mode_test/DataFiles.test.cc
@@ -24,6 +24,10 @@ int compareFiles(const std::string& p1, const std::string& p2) {
     std::istreambuf_iterator<char>(f2.rdbuf()))) return 2;
   return 3;
 }
+int test_fun() {
+  static int val = 10;
+  return val += 3;
+}
 
 TEST_CASE("WriteOrgDumpFile", "[default]") {
   emp::Random random(17);
@@ -37,4 +41,43 @@ TEST_CASE("WriteOrgDumpFile", "[default]") {
   world.WriteOrgDumpFile(test_file_name);
   
   REQUIRE(compareFiles(test_file_name, "source/test/default_mode_test/test_data_files/org_dump_file.txt") == 2);
+}
+
+TEST_CASE("Test DataFile", "[default]") {
+  int test_int = 5;
+
+  emp::DataFile dfile("new_test_file.dat");
+
+  REQUIRE(dfile.GetFilename() == "new_test_file.dat");
+
+  emp::DataMonitor<int> data_squares;
+  emp::DataMonitor<uint64_t> data_cubes;
+
+  dfile.AddCurrent(data_squares);
+  dfile.AddCurrent(data_cubes);
+  dfile.AddTotal(data_cubes);
+  dfile.AddMin(data_cubes);
+  dfile.AddMax(data_cubes);
+  dfile.AddFun<int>(test_fun);
+  dfile.AddVar<int>(test_int);
+
+  for (size_t i = 0; i < 10; i++) {
+    test_int += i;
+    data_squares.Add((int)(i * i));
+    data_cubes.Add(i * i * i);
+    dfile.Update();
+
+    // std::cout << i << std::endl;
+  }
+
+  dfile.SetupLine("[[", ":", "]]\n");
+  for (size_t i = 10; i < 20; i++) {
+    data_squares.Add((int)(i * i));
+    data_cubes.Add(i * i * i);
+    dfile.Update();
+
+    // std::cout << i << std::endl;
+  }
+
+  REQUIRE(compareFiles("new_test_file.dat", "source/test/default_mode_test/test_file.dat") == 2);
 }
